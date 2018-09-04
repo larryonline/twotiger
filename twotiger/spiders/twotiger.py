196a72389ceff9d317cad213e591a45b20c41b77
larry <larry.zzn@gmail.com>
2018-09-04 16:25:48 +0800

    setup spider

--- twotiger/spiders/twotiger.py
+++ twotiger/spiders/twotiger.py
@@ -4,19 +4,44 @@ import scrapy
 
 class TwoTigerSpider(scrapy.Spider):
     name = "TwoTiger"
-    allowed_domains = ["books.toscrape.com"]
+    allowed_domains = ["twotiger.com"]
     start_urls = [
-        'http://books.toscrape.com/',
+        'https://www.twotiger.com/bond/-1',
     ]
 
     def parse(self, response):
-        for book_url in response.css("article.product_pod > h3 > a ::attr(href)").extract():
-            yield scrapy.Request(response.urljoin(book_url), callback=self.parse_book_page)
-        next_page = response.css("li.next > a ::attr(href)").extract_first()
+
+        for item_url in response.css("div.hotItems div.hotItemsListInfo a ::attr(href)").extract():
+            yield scrapy.Request(response.urljoin(item_url), callback=self.parse_touzi_page)
+        next_page = response.css("div.page > ul > li.right > a ::attr(href)").extract_first()
+
         if next_page:
             yield scrapy.Request(response.urljoin(next_page), callback=self.parse)
 
+        #
+        # for book_url in response.css("article.product_pod > h3 > a ::attr(href)").extract():
+        #    yield scrapy.Request(response.urljoin(book_url), callback=self.parse_book_page)
+        #next_page = response.css("li.next > a ::attr(href)").extract_first()
+        #if next_page:
+        #    yield scrapy.Request(response.urljoin(next_page), callback=self.parse)
+
+    def parse_touzi_page(self, response):
+        item = {};
+
+        nameInfo = response.css("div.nameInfo");
+        item["name"] = nameInfo.css("p").extract_first();
+        item["discount"] = nameInfo.css("em.dz").extract_first();
+
+        item["type"] = response.css("div.projectName em.doubt_no").extract_first();
+
+        volumeInfo = response.css("div.lijitouzi");
+        item["volume"] = volumeInfo.css("p > span.fr").extract_first();
+        item["profit"] = volumeInfo.css("p > span.yjsy").extract_first();
+
+        yield item
+
     def parse_book_page(self, response):
+        pass
         item = {}
         product = response.css("div.product_main")
         item["title"] = product.css("h1 ::text").extract_first()
